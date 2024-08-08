d8f5753aae89a106db0ec794f1e1d40d11e2e80b
maheen562 <maheenkhan193@gmail.com>
2024-08-08 12:44:11 +1000

    added promotion sticker

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,7 +1,8 @@
 import React from "react";
 import "./Products.css";
 import prod1 from "../assets/2DF_200.jpg";
-
+import prod2 from "../assets/3DF_200.jpg"
+import Sticker from "../assets/50.png"
 const vitaminProducts = [
   {
     productImage: prod1,
@@ -9,7 +10,7 @@ const vitaminProducts = [
     price: "17.09",
     discounted: true,
     priceDiscounted: "17.00",
-    promotionSticker: false,
+    promotionSticker: true,
     description: [
       "Contains Calcium Carbonate, Vitamin D3",
       "Increased Bone and joint health",
@@ -17,17 +18,16 @@ const vitaminProducts = [
       "150 days supply",
     ],
   },{
-    productImage: prod1,
-    name: "Swisse Ultiboost Calcium+Vitamin D 150 Tablets",
-    price: "17.09",
-    discounted: true,
-    priceDiscounted: "17.00",
+    productImage: prod2,
+    name: "Elevit Pre-conception & Pregnancy Multivitamin 100 Tablets (100 Days)",
+    price: "59.00",
+    discounted: false,
+    priceDiscounted: "",
     promotionSticker: false,
     description: [
-      "Contains Calcium Carbonate, Vitamin D3",
-      "Increased Bone and joint health",
-      "Reduces the risk of muscle cramps and spasms",
-      "150 days supply",
+      "Contains  Vitamin  B12,  Iron ,  Omega 3-DHA",
+      "100 Days Supply",
+      "Provides Key nutrients for a mothers health",
     ],
   },
 ];
@@ -45,10 +45,11 @@ function Products() {
           {vitaminProducts.map((product, pindex) => (
             <li id={pindex}>
               <div className="product-padding">
+                
                 <div className="product" id="prod1">
                   <img src={product.productImage} alt="prod1" />
                   <h4>{product.name}</h4>
-                  <span className="product-price">{product.price}</span>
+                  <span className="product-price">${product.price}</span>
                   {product.discounted && (
                     <div className="promotion">
                       <p>
@@ -66,6 +67,9 @@ function Products() {
                   </div>
                   <button className="buy-now-btn">Buy now </button>
                 </div>
+                {product.promotionSticker && (
+                  <img className="sticker-50" src={Sticker}/>
+                )}
               </div>
             </li>
           ))}
